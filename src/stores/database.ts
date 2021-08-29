f5cf78e58f4eee1ea4fe320aa06811134df6e3c3
chuksdozie <chuksdozie48@gmail.com>
2021-08-29 15:26:09 +0100

    setting a single lock file

--- src/stores/database.ts
+++ src/stores/database.ts
@@ -1,24 +1,27 @@
-import postgres from 'postgres';
+import postgres from "postgres";
 
-const IN_PROD = process.env.NODE_ENV === 'production';
+const IN_PROD = process.env.NODE_ENV === "production";
 
 export const sql = postgres(process.env.DATABASE_URL!, {
   debug: (_, query, params) => {
     if (!IN_PROD) {
-      const queryString = query.split('\n').join('');
+      const queryString = query.split("\n").join("");
       console.log({ query: queryString, params });
     }
+  },
+  ssl: {
+    rejectUnauthorized: false,
   },
 });
 
 export function testDBConnection() {
   sql`SELECT 1+1 AS result`
-    .then(() => console.log('Connected to postgres'))
+    .then(() => console.log("Connected to postgres"))
     .catch((err) => {
       if (!IN_PROD) throw Error(err);
     });
 }
 
 export function closeDBConnection() {
-  sql.end().then(() => console.log('Connection to Postgres closed'));
+  sql.end().then(() => console.log("Connection to Postgres closed"));
 }

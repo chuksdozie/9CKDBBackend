312ee539d4fc5114cfdd11a20a4289b9901e2daa
chuksdozie <chuksdozie48@gmail.com>
2021-08-30 02:24:22 +0100

    adding the heroku run file

---
+++ .heroku/run.sh
@@ -0,0 +1,8 @@
+yarn migrate up
+yarn tsc
+
+###### There is currently no client side, uncomment when there is a client side #########
+# cd client
+# export SKIP_PREFLIGHT_CHECK=true
+# yarn
+# yarn build
